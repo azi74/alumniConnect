5259383750a95058ec1b4d303fe339e3363275a6
asi <aboobackerasi198@gmail.com>
2025-05-27 16:29:23 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 
 // ====== Global Middleware ======
 app.use(cors({
-  origin: 'https://alumni-connect-gamma.vercel.app/',
+  origin: 'https://alumni-connect-gamma.vercel.app',
   credentials: true,
 }));
 
